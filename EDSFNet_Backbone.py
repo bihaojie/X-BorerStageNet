6f2a65a7b8efa5ec525a1f551b765d2ce418a1f7
bihaojie <81062023+bihaojie@users.noreply.github.com>
2026-06-10 10:19:10 +0800

    Update EDSFNet_Backbone.py

--- EDSFNet_Backbone.py
+++ EDSFNet_Backbone.py
@@ -1,5 +1,5 @@
 backbone:
-  - [-1, APSD, [16, 16, True]] 
+  - [-1, APSDM, [16, 16, True]] 
   - [-1, DSHG, [16, 64, 1, 3, False, False, 3, True, 'se']] 
   - [-1, DSHG, [32, 256, 1, 3, True, False, 3, True, 'se']] 
   - [-1, DSHG, [64, 512, 2, 3, True, True, 5, True, 'se']]  
